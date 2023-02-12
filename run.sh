111cd8b58e7f88edf21f62db234058a608101608
zhujq2021 <80183504+zhujq2021@users.noreply.github.com>
2023-02-12 23:57:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,13 +10,14 @@ chmod 600 /root/.ssh/authorized_keys
 nohup /usr/sbin/sshd -D &
 echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;35;35m\]\w\[\033[00m\]\$\033[1;32;32m\] '"'" >> /root/.bashrc
 
-mkdir -p /root/tail
-cd /root/tail
-wget https://pkgs.tailscale.com/stable/${TSFILE}
-tar xzf ${TSFILE} --strip-components=1
-mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
-nohup ./tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-./tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=railway-vps
+
+#mkdir -p /root/tail
+#cd /root/tail
+#wget https://pkgs.tailscale.com/stable/${TSFILE}
+#tar xzf ${TSFILE} --strip-components=1
+#mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
+#nohup ./tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+#./tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=railway-vps
 
 mkdir -p /v2ray
 cd /v2ray
